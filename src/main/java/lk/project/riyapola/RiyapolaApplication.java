5fb1e0d3fe525df10dc9f51af6c7c57462da6032
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-03-12 14:17:42 +0530

    first commit

---
+++ src/main/java/lk/project/riyapola/RiyapolaApplication.java
@@ -0,0 +1,13 @@
+package lk.project.riyapola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiyapolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiyapolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/project/riyapola/RiyapolaApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.project.riyapola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiyapolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
