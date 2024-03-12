d02d0bab623b03831367ec58b3a3ee3ddc2d52e3
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-03-12 15:30:44 +0530

    create Vehicle class using lombok

---
+++ src/main/java/lk/project/riyapola/entity/Vehicle.java
@@ -0,0 +1,25 @@
+package lk.project.riyapola.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer vehicleNo;
+    private String brand;
+    private String model;
+    private String description;
+    private double price;
+}
